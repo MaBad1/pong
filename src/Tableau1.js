1c86d83de61429707f8f6a3b9c7e5c5ecebcb342
MaBad1 <abadiemathieu13@gmail.com>
2022-01-27 16:13:44 +0100

    baby pong 1

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -1,5 +1,7 @@
 class Tableau1 extends Phaser.Scene{
     preload(){
+        //Preload de tous les assets.
+
         this.load.image('cercle',"assets/cercle.png");
         this.load.image('carre',"assets/carre.png");
         this.load.image('back',"assets/back.png");
@@ -13,14 +15,20 @@ class Tableau1 extends Phaser.Scene{
         this.hauteur=500;
         this.largeur=1000;
 
+        //Background
+
         this.bg=this.add.sprite(500,250,'back');
         this.bg.setDisplaySize(1100,500);
+
+        //Sprite de la balle et paramètres
 
         this.balle=this.physics.add.sprite(this.largeur/2,this.hauteur/2,'cercle');
         this.balle.setDisplaySize(20,20);
         this.balle.body.setBounce(1.1,1.1);
         this.balle.body.setMaxVelocity(500,500);
         this.Initiale();
+
+        //Limites haute et basse du terrain
 
         this.haut=this.physics.add.sprite(0,0,'carre').setOrigin(0,0);
         this.haut.setDisplaySize(this.largeur,20);
@@ -32,15 +40,37 @@ class Tableau1 extends Phaser.Scene{
         this.bas.body.setAllowGravity(false);
         this.bas.setImmovable(true);
 
+        //raquettes gauches
+
         this.gauche=this.physics.add.sprite(0, (this.hauteur/2)-50,'gauche').setOrigin(0,0);
         this.gauche.setDisplaySize(50, 100);
         this.gauche.setImmovable(true);
+
+        this.gauche2=this.physics.add.sprite(300, 50,'gauche').setOrigin(0,0);
+        this.gauche2.setDisplaySize(50, 100);
+        this.gauche2.setImmovable(true);
+
+        this.gauche3=this.physics.add.sprite(300, this.hauteur-150,'gauche').setOrigin(0,0);
+        this.gauche3.setDisplaySize(50, 100);
+        this.gauche3.setImmovable(true);
+
+        //raquettes droites
 
         this.droite=this.physics.add.sprite(this.largeur-50, (this.hauteur/2)-50,'droite').setOrigin(0,0);
         this.droite.setDisplaySize(50, 100);
         this.droite.setImmovable(true);
 
+        this.droite2=this.physics.add.sprite(this.largeur-350, 50,'droite').setOrigin(0,0);
+        this.droite2.setDisplaySize(50, 100);
+        this.droite2.setImmovable(true);
+
+        this.droite3=this.physics.add.sprite(this.largeur-350, this.hauteur-150,'droite').setOrigin(0,0);
+        this.droite3.setDisplaySize(50, 100);
+        this.droite3.setImmovable(true);
+
         let me=this;
+
+        //colliders
 
         this.physics.add.collider(this.balle,this.bas);
         this.physics.add.collider(this.balle,this.haut);
@@ -48,20 +78,31 @@ class Tableau1 extends Phaser.Scene{
         this.physics.add.collider(this.balle,this.gauche, function (){
             me.rebond(me.gauche);
         });
+        this.physics.add.collider(this.balle,this.gauche2, function (){
+            me.rebond(me.gauche2);
+        });
+        this.physics.add.collider(this.balle,this.gauche3, function (){
+            me.rebond(me.gauche3);
+        });
+
         this.physics.add.collider(this.balle,this.droite, function (){
             me.rebond(me.droite);
         });
+        this.physics.add.collider(this.balle,this.droite2, function (){
+            me.rebond(me.droite2);
+        });
+        this.physics.add.collider(this.balle,this.droite3, function (){
+            me.rebond(me.droite3);
+        });
 
-        this.physics.add.collider(this.haut, this.gauche);
-        this.physics.add.collider(this.bas, this.gauche);
 
-        this.physics.add.collider(this.haut, this.droite);
-        this.physics.add.collider(this.bas, this.droite);
-
+        //joueurs
 
         this.joueurGauche = new Joueur('Guingamp','joueurGauche')
         this.joueurDroite = new Joueur('FC Bourdeaux','joueurDroite')
         console.log(this.joueurGauche)
+
+        //Particules
 
         this.particles = this.add.particles('flame');
         this.blueparticles = this.add.particles('blue');
@@ -81,6 +122,7 @@ class Tableau1 extends Phaser.Scene{
     }
 
     Initiale (){
+        //fonction pour initialiser la vitesse et la direction de la balle au début d'un round
         this.balle.setX(this.largeur/2);
         this.balle.setY(this.hauteur/2);
 
@@ -123,18 +165,103 @@ class Tableau1 extends Phaser.Scene{
         let me=this;
         this.input.keyboard.on('keydown', function(kevent) {
 
+            //inputs pour avoir un movement et un arrêt simultané des raquettes en gardant un même écart entre les raquettes du milieu de terrain.
+
             switch (kevent.keyCode) {
                 case Phaser.Input.Keyboard.KeyCodes.S:
-                    me.gauche.setVelocityY(-400);
+                    if (me.gauche.y>20){
+                        me.gauche.setVelocityY(-400)
+                    }
+                    else{
+                        me.gauche.setY(20)
+                        me.gauche.setVelocityY(0)
+                    }
+                    if (me.gauche2.y>20){
+                        me.gauche2.setVelocityY(-200)
+                    }
+                    else{
+                        me.gauche2.setY(20)
+                        me.gauche2.setVelocityY(0)
+                    }
+                    if (me.gauche2.y>20){
+                        me.gauche3.setVelocityY(-200)
+                    }
+                    else{
+                        me.gauche3.setY(320)
+                        me.gauche3.setVelocityY(0)
+                    }
                     break;
+
                 case Phaser.Input.Keyboard.KeyCodes.X:
-                    me.gauche.setVelocityY(400);
+                    if (me.gauche.y<380){
+                        me.gauche.setVelocityY(400)
+                    }
+                    else{
+                        me.gauche.setVelocityY(0)
+                        me.gauche.setY(380)
+                    }
+                    if (me.gauche3.y<380){
+                        me.gauche2.setVelocityY(200)
+                    }
+                    else{
+                        me.gauche2.setVelocityY(0)
+                        me.gauche2.setY(80)
+                    }
+                    if (me.gauche3.y<380){
+                        me.gauche3.setVelocityY(200)
+                    }
+                    else{
+                        me.gauche3.setVelocityY(0)
+                        me.gauche3.setY(380)
+                    }
                     break;
+
                 case Phaser.Input.Keyboard.KeyCodes.J:
-                    me.droite.setVelocityY(-400);
+                    if (me.droite.y>20){
+                        me.droite.setVelocityY(-400)
+                    }
+                    else{
+                        me.droite.setY(20)
+                        me.droite.setVelocityY(0)
+                    }
+                    if (me.droite2.y>20){
+                        me.droite2.setVelocityY(-200)
+                    }
+                    else{
+                        me.droite2.setY(20)
+                        me.droite2.setVelocityY(0)
+                    }
+                    if (me.droite2.y>20){
+                        me.droite3.setVelocityY(-200)
+                    }
+                    else{
+                        me.droite3.setY(320)
+                        me.droite3.setVelocityY(0)
+                    }
                     break;
+
                 case Phaser.Input.Keyboard.KeyCodes.N:
-                    me.droite.setVelocityY(400);
+                    if (me.droite.y<380){
+                        me.droite.setVelocityY(400)
+                    }
+                    else{
+                        me.droite.setVelocityY(0)
+                        me.droite.setY(380)
+                    }
+                    if (me.droite3.y<380){
+                        me.droite2.setVelocityY(200)
+                    }
+                    else{
+                        me.droite2.setVelocityY(0)
+                        me.droite2.setY(80)
+                    }
+                    if (me.droite3.y<380){
+                        me.droite3.setVelocityY(200)
+                    }
+                    else{
+                        me.droite3.setVelocityY(0)
+                        me.droite3.setY(380)
+                    }
                     break;
             }
         });
@@ -143,15 +270,23 @@ class Tableau1 extends Phaser.Scene{
             switch (kevent.keyCode) {
                 case Phaser.Input.Keyboard.KeyCodes.S:
                     me.gauche.setVelocityY(0);
+                    me.gauche2.setVelocityY(0);
+                    me.gauche3.setVelocityY(0);
                     break;
                 case Phaser.Input.Keyboard.KeyCodes.X:
                     me.gauche.setVelocityY(0);
+                    me.gauche2.setVelocityY(0);
+                    me.gauche3.setVelocityY(0);
                     break;
                 case Phaser.Input.Keyboard.KeyCodes.J:
                     me.droite.setVelocityY(0);
+                    me.droite2.setVelocityY(0);
+                    me.droite3.setVelocityY(0);
                     break;
                 case Phaser.Input.Keyboard.KeyCodes.N:
                     me.droite.setVelocityY(0);
+                    me.droite2.setVelocityY(0);
+                    me.droite3.setVelocityY(0);
                     break;
             }
         });
@@ -160,7 +295,11 @@ class Tableau1 extends Phaser.Scene{
     update(){
         if(this.balle.x > this.largeur){
             this.gauche.setY((this.hauteur/2)-50)
+            this.gauche2.setY(50)
+            this.gauche3.setY(this.hauteur-150)
             this.droite.setY((this.hauteur/2)-50)
+            this.droite2.setY(50)
+            this.droite3.setY(this.hauteur-150)
 
             this.emitter = this.add.particles('red').createEmitter({
                 x : this.largeur,
@@ -178,7 +317,11 @@ class Tableau1 extends Phaser.Scene{
         }
         if(this.balle.x <0){
             this.gauche.setY((this.hauteur/2)-50)
+            this.gauche2.setY(50)
+            this.gauche3.setY(this.hauteur-150)
             this.droite.setY((this.hauteur/2)-50)
+            this.droite2.setY(50)
+            this.droite3.setY(this.hauteur-150)
 
             this.emitter = this.add.particles('blue').createEmitter({
                 x : 0,
